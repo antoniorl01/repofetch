8b81ae41c2a56b5afe8e14141173714ba2e7482d
antonio rubia latorre <antonio.rl.1011@gmail.com>
2024-07-20 21:52:09 +0200

    Created struct, added dependency for skyscrapper

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use reqwest::get;
+use serde::{Serialize, Deserialize};
+use skyscraper::html::{self, parse::ParseError};
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RepositoryData {
+  pub project: String,
+  pub head: String,
+  pub pending: String,
+  pub version: String,
+  pub created: String,
+  pub languages: String,
+  pub dependencies: String,
+  pub authors: String,
+  pub last_changes: String,
+  pub contributors: String,
+  pub repo: String,
+  pub commits: String,
+  pub lines_of_code: String,
+  pub size: String,
+  pub license: String,
+
 }
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let args: Vec<String> = env::args().collect();
+
+  if args.len() < 2 {
+    panic!("NO REPOSITORY WAS PROVIDED");
+  }
+
+  let url = &args[1];
+  let resp = get(url).await?.text().await?;
+  let document = html::parse(&*resp);
+
+  // res to string
+  // string to html::parse
+  // html to RepositoryData
+  // repository data to imprimir x pantalla
+  // let document = html::parse(html_text)?;
+
+  println!("{:?}", args);
+
+  Ok(())
+}
+
