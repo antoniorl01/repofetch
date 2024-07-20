911974511452491dd5e4f269d12e0b57b08dc6fc
antonio rubia latorre <antonio.rl.1011@gmail.com>
2024-07-21 00:09:14 +0200

    Added some calls for GitHub api

---
+++ src/get_repository_commits.rs
@@ -0,0 +1,152 @@
+use serde_derive::Deserialize;
+use serde_derive::Serialize;
+use serde_json::Value;
+
+pub type Root = Vec<Root2>;
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Root2 {
+    pub sha: String,
+    #[serde(rename = "node_id")]
+    pub node_id: String,
+    pub commit: Commit,
+    pub url: String,
+    #[serde(rename = "html_url")]
+    pub html_url: String,
+    #[serde(rename = "comments_url")]
+    pub comments_url: String,
+    pub author: Author2,
+    pub committer: Committer2,
+    pub parents: Vec<Parent>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Commit {
+    pub author: Author,
+    pub committer: Committer,
+    pub message: String,
+    pub tree: Tree,
+    pub url: String,
+    #[serde(rename = "comment_count")]
+    pub comment_count: i64,
+    pub verification: Verification,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Author {
+    pub name: String,
+    pub email: String,
+    pub date: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Committer {
+    pub name: String,
+    pub email: String,
+    pub date: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Tree {
+    pub sha: String,
+    pub url: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Verification {
+    pub verified: bool,
+    pub reason: String,
+    pub signature: Value,
+    pub payload: Value,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Author2 {
+    pub login: String,
+    pub id: i64,
+    #[serde(rename = "node_id")]
+    pub node_id: String,
+    #[serde(rename = "avatar_url")]
+    pub avatar_url: String,
+    #[serde(rename = "gravatar_id")]
+    pub gravatar_id: String,
+    pub url: String,
+    #[serde(rename = "html_url")]
+    pub html_url: String,
+    #[serde(rename = "followers_url")]
+    pub followers_url: String,
+    #[serde(rename = "following_url")]
+    pub following_url: String,
+    #[serde(rename = "gists_url")]
+    pub gists_url: String,
+    #[serde(rename = "starred_url")]
+    pub starred_url: String,
+    #[serde(rename = "subscriptions_url")]
+    pub subscriptions_url: String,
+    #[serde(rename = "organizations_url")]
+    pub organizations_url: String,
+    #[serde(rename = "repos_url")]
+    pub repos_url: String,
+    #[serde(rename = "events_url")]
+    pub events_url: String,
+    #[serde(rename = "received_events_url")]
+    pub received_events_url: String,
+    #[serde(rename = "type")]
+    pub type_field: String,
+    #[serde(rename = "site_admin")]
+    pub site_admin: bool,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Committer2 {
+    pub login: String,
+    pub id: i64,
+    #[serde(rename = "node_id")]
+    pub node_id: String,
+    #[serde(rename = "avatar_url")]
+    pub avatar_url: String,
+    #[serde(rename = "gravatar_id")]
+    pub gravatar_id: String,
+    pub url: String,
+    #[serde(rename = "html_url")]
+    pub html_url: String,
+    #[serde(rename = "followers_url")]
+    pub followers_url: String,
+    #[serde(rename = "following_url")]
+    pub following_url: String,
+    #[serde(rename = "gists_url")]
+    pub gists_url: String,
+    #[serde(rename = "starred_url")]
+    pub starred_url: String,
+    #[serde(rename = "subscriptions_url")]
+    pub subscriptions_url: String,
+    #[serde(rename = "organizations_url")]
+    pub organizations_url: String,
+    #[serde(rename = "repos_url")]
+    pub repos_url: String,
+    #[serde(rename = "events_url")]
+    pub events_url: String,
+    #[serde(rename = "received_events_url")]
+    pub received_events_url: String,
+    #[serde(rename = "type")]
+    pub type_field: String,
+    #[serde(rename = "site_admin")]
+    pub site_admin: bool,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Parent {
+    pub sha: String,
+    pub url: String,
+    #[serde(rename = "html_url")]
+    pub html_url: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,37 @@
+mod get_repository_response;
+mod get_repository_commits;
+mod get_repository_languages;
+
 use std::env;
 use reqwest::get;
 use serde::{Serialize, Deserialize};
-use skyscraper::html::{self, parse::ParseError};
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct RepositoryData {
-  pub project: String,
-  pub head: String,
-  pub pending: String,
-  pub version: String,
-  pub created: String,
-  pub languages: String,
-  pub dependencies: String,
-  pub authors: String,
-  pub last_changes: String,
-  pub contributors: String,
-  pub repo: String,
-  pub commits: String,
-  pub lines_of_code: String,
-  pub size: String,
-  pub license: String,
 
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-  let args: Vec<String> = env::args().collect();
+  let mut args: Vec<String> = env::args().collect();
+  args.push("https://api.github.com/repos/antoniorl01/repofetch".to_owned());
 
   if args.len() < 2 {
     panic!("NO REPOSITORY WAS PROVIDED");
   }
 
   let url = &args[1];
-  let resp = get(url).await?.text().await?;
-  let document = html::parse(&*resp);
+  let resp = get(url).await?;
+  let repo_data: get_repository_response::Root = resp.json().await?;
 
-  // res to string
-  // string to html::parse
-  // html to RepositoryData
-  // repository data to imprimir x pantalla
-  // let document = html::parse(html_text)?;
+  // problema, aqui cambian las propiedades
+  let resp = get(&url.to_owned().push_str("/languages")).await?;
 
-  println!("{:?}", args);
+  let resp = get(&url.to_owned().push_str("/commits")).await?;
+  let commits: get_repository_commits::Root = resp.json().await?;
+  let commit_num = commits.len();
 
   Ok(())
 }
+
+// https://api.github.com/repos/antoniorl01/repofetch
+// https://api.github.com/repos/antoniorl01/repofetch/languages
+// https://api.github.com/repos/antoniorl01/repofetch/commits
 
